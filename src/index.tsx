9ce051178a09401c5f39e52d6261abf5776b4cbf
gh0stonio <antoine.leveque.pro@gmail.com>
2019-12-16 22:41:55 +0100

    chore: implement linter, docz etc

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div``
+
+const Swippy: React.FC = () => {
+  return <Wrapper>swippy</Wrapper>
+}
+
+export default Swippy
