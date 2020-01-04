f5ad7637d51e159429802fe513aefbd01f9a3099
gh0stonio <antoine.leveque.pro@gmail.com>
2020-01-04 22:12:26 +0100

    build: add build script and update release script

--- src/styles.tsx
+++ src/styles.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const Wrapper = styled.div.attrs(({ width }: { width?: number }): any => ({
   style: {
     width: `${width}px` || '100%',
@@ -10,6 +11,7 @@ export const Wrapper = styled.div.attrs(({ width }: { width?: number }): any => 
   user-select: none;
 `
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const Container = styled.div.attrs(({ delta }: { delta: number }): any => ({
   style: {
     transform: `translateX(${delta}px)`,
